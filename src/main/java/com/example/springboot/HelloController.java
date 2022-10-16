56fad266cccd168c3affd80710fe8f3a80c8c235
Wonjo Yoo <wonjoyoo@gmail.com>
2022-10-16 20:02:08 +0900

    new

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -1,15 +1,53 @@
 package com.example.springboot;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@RestController
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.client.RestTemplate;
+
+
+@Controller
 public class HelloController {
 
-	@RequestMapping("/")
-	public String index() {
-		System.out.println("무엇이 문제일까");
-		return "Greetings from Spring Boot + Tanzu!. live update. oh my god. good.";
+	@GetMapping("/")
+	public String index( Model model) {
+		return "index";
+	}
+
+	@GetMapping("/greeting")
+	public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+		System.out.println("param name=" + name);
+		model.addAttribute("name", name);
+		return "greeting";
+	}
+
+	@GetMapping("/getdata")
+	public String getdata(@RequestHeader("User-Agent") String userAgent, Model model) {
+		String result = null ;
+		if (userAgent.indexOf("Chrome") != -1 ){
+			String url = "http://node-express.default.dev.abc.tanzukorea.xyz/";
+			RestTemplate restTemplate = new RestTemplate();
+			result = restTemplate.getForObject(url, String.class);
+		}
+		model.addAttribute("result", result.length());
+		return "getdata";
+	}
+
+	@GetMapping("/sleep")
+	public String sleep(@RequestParam(name="time", required=false, defaultValue="1") long time, Model model) {
+		System.out.println("Sleeping duration =" + time);
+		try {
+			Thread.sleep(time * 1000);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		System.out.println("Wake up duration =" + time);
+		model.addAttribute("time", time);
+		return "sleep";
 	}
 
 }
